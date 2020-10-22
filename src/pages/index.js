05d17374838fc4b16b3f2b47e05be0d2fdd03b65
Vaughan Stedman <vaughan.stedman@gmail.com>
2020-10-22 14:21:52 +1100

    feat(web): add deployability and update favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
-
+import { Helmet } from "react-helmet"
+import styles from "./index.module.css"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className={styles.container}>
+      <Helmet>
+        <title>Backyard Biodiversity</title>
+      </Helmet>
+      <div className={styles.centeredContent}>
+        <img
+          className={styles.image}
+          alt="logo"
+          src={require("../../static/logo_512.png")}
+        />
+        <h1>Coming soon</h1>
+      </div>
+    </div>
+  )
 }
